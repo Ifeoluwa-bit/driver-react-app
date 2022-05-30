840ed4019ccddea3848150500c2f18b5253763d3
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-30 03:42:47 -0700

    Add files via upload

---
+++ src/components/Handling/Handling.js
@@ -0,0 +1,22 @@
+import './Handling.css'
+
+export default function Handling() {
+  return (
+    <div name='handling' className='handling'>
+
+        <div className="container">
+
+        <div className="top">
+            <h1>Handling</h1>
+        </div>
+
+        <div className="bottom">
+            <button className="btn btn-dark">Drive</button>
+            <button className="btn">Ride</button>
+        </div>
+
+        </div>
+
+    </div>
+  )
+}
