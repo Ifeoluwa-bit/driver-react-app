ee9f68d3eee77f4d0686b2dbbcbe619a724f627e
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-30 03:38:04 -0700

    Add files via upload

---
+++ src/components/Speed/Speed.js
@@ -0,0 +1,22 @@
+import './Speed.css'
+
+export default function Speed() {
+  return (
+    <div name='speed' className='speed'>
+
+        <div className="container">
+
+        <div className="top">
+            <h1>Speed</h1>
+        </div>
+
+        <div className="bottom">
+            <button className="btn btn-dark">Drive</button>
+            <button className="btn">Ride</button>
+        </div>
+
+        </div>
+
+    </div>
+  )
+}
