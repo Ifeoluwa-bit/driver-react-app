8f2de3ac28811e0425396d1bb0a82ddf828ece2b
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-30 03:38:58 -0700

    Add files via upload

---
+++ src/components/Power/Power.js
@@ -0,0 +1,22 @@
+import './Power.css'
+
+export default function Power() {
+  return (
+    <div name='power' className='power'>
+
+        <div className="container">
+
+            <div className="top">
+                <h1>Power</h1>
+            </div>
+
+            <div className="bottom">
+                <button className="btn btn-dark">Drive</button>
+                <button className="btn">Ride</button>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
